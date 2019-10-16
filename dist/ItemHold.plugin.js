42e4bdd8c44afd95f8af007b5621a8fc921bc9ec
neuronet.io <neuronet.io@gmail.com>
2019-10-16 16:57:25 +0200

    build

--- dist/ItemHold.plugin.js
+++ dist/ItemHold.plugin.js
@@ -1,99 +1,10 @@
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global = global || self, global.ItemHold = factory());
-}(this, function () { 'use strict';
-
-  /**
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):(e=e||self).ItemHold=t()}(this,(function(){"use strict";
+/**
    * Gantt-Schedule-Timeline-Calendar
    *
    * @copyright Rafal Pospiech <https://neuronet.io>
    * @author    Rafal Pospiech <neuronet.io@gmail.com>
    * @package   gantt-schedule-timeline-calendar
    * @license   GPL-3.0
-   */
-
-  function ItemHoldPlugin(options = {}) {
-    const defaultOptions = {
-      time: 1000,
-      movementThreshold: 2,
-      action(element, data) {}
-    };
-    options = { ...defaultOptions, ...options };
-
-    const holding = {};
-    const mouse = { x: 0, y: 0 };
-
-    function onMouseDown(item, element, event) {
-      if (typeof holding[item.id] === 'undefined') {
-        holding[item.id] = { x: event.x, y: event.y };
-        setTimeout(() => {
-          if (typeof holding[item.id] !== 'undefined') {
-            let exec = true;
-            let xMovement = holding[item.id].x - mouse.x;
-            if (Math.sign(xMovement) === -1) {
-              xMovement = -xMovement;
-            }
-            let yMovement = holding[item.id].y - mouse.y;
-            if (Math.sign(yMovement) === -1) {
-              yMovement = -yMovement;
-            }
-            if (xMovement > options.movementThreshold) {
-              exec = false;
-            }
-            if (yMovement > options.movementThreshold) {
-              exec = false;
-            }
-            delete holding[item.id];
-            if (exec) {
-              options.action(element, item);
-            }
-          }
-        }, options.time);
-      }
-    }
-
-    function onMouseUp(itemId) {
-      if (typeof holding[itemId] !== 'undefined') {
-        delete holding[itemId];
-      }
-    }
-
-    const mouseUps = [];
-    const mouseMoves = [];
-
-    function action(element, data) {
-      element.addEventListener('mousedown', event => {
-        onMouseDown(data.item, element, event);
-      });
-      function mouseUp() {
-        onMouseUp(data.item.id);
-      }
-      mouseUps.push(mouseUp);
-      document.addEventListener('mouseup', mouseUp);
-      function mouseMove(event) {
-        mouse.x = event.x;
-        mouse.y = event.y;
-      }
-      mouseMoves.push(mouseMove);
-      document.addEventListener('mousemove', mouseMove);
-      return {
-        destroy(element, data) {
-          mouseUps.forEach(mouseUp => document.removeEventListener('mouseup', mouseUp));
-          mouseMoves.forEach(mouseMove => document.removeEventListener('mousemove', mouseMove));
-        }
-      };
-    }
-
-    return function initializePlugin(state, api) {
-      state.update('config.actions.chart-gantt-items-row-item', actions => {
-        actions.push(action);
-        return actions;
-      });
-    };
-  }
-
-  return ItemHoldPlugin;
-
-}));
+   */return function(e={}){e={...{time:1e3,movementThreshold:2,action(e,t){}},...e};const t={},n={x:0,y:0},o=[],i=[];function d(d,u){function m(){var e;e=u.item.id,void 0!==t[e]&&delete t[e]}function s(e){n.x=e.x,n.y=e.y}return d.addEventListener("mousedown",o=>{!function(o,i,d){void 0===t[o.id]&&(t[o.id]={x:d.x,y:d.y},setTimeout(()=>{if(void 0!==t[o.id]){let d=!0,u=t[o.id].x-n.x;-1===Math.sign(u)&&(u=-u);let m=t[o.id].y-n.y;-1===Math.sign(m)&&(m=-m),u>e.movementThreshold&&(d=!1),m>e.movementThreshold&&(d=!1),delete t[o.id],d&&e.action(i,o)}},e.time))}(u.item,d,o)}),o.push(m),document.addEventListener("mouseup",m),i.push(s),document.addEventListener("mousemove",s),{destroy(e,t){o.forEach(e=>document.removeEventListener("mouseup",e)),i.forEach(e=>document.removeEventListener("mousemove",e))}}}return function(e,t){e.update("config.actions.chart-gantt-items-row-item",e=>(e.push(d),e))}}}));
 //# sourceMappingURL=ItemHold.plugin.js.map
