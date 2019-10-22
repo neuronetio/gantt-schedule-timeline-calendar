1d95e3cb85a7ed165a916177cba6bf46d8e62bb3
neuronet.io <neuronet.io@gmail.com>
2019-10-22 15:32:38 +0200

    non static internal state

--- src/index.ts
+++ src/index.ts
@@ -11,53 +11,52 @@ import Vido from '@neuronet.io/vido';
 import publicApi, { getInternalApi } from './api/Api';
 import Main from './components/Main';
 
-const _internal = {
-  components: {
-    Main
-  },
-  scrollBarHeight: 17,
-  height: 0,
-  treeMap: {},
-  flatTreeMap: [],
-  flatTreeMapById: {},
-  list: {
-    expandedHeight: 0,
-    visibleRows: [],
-    rows: {},
-    width: 0
-  },
-  dimensions: {
-    width: 0,
-    height: 0
-  },
-  chart: {
-    dimensions: {
-      width: 0,
-      innerWidth: 0
-    },
-    visibleItems: [],
-    time: {
-      dates: {},
-      timePerPixel: 0,
-      firstTaskTime: 0,
-      lastTaskTime: 0,
-      totalViewDurationMs: 0,
-      totalViewDurationPx: 0,
-      leftGlobal: 0,
-      rightGlobal: 0,
-      leftPx: 0,
-      rightPx: 0,
-      leftInner: 0,
-      rightInner: 0,
-      maxWidth: {}
-    }
-  },
-  elements: {}
-};
-
 const GSTC = options => {
   const state = options.state;
   const api = getInternalApi(state);
+  const _internal = {
+    components: {
+      Main
+    },
+    scrollBarHeight: 17,
+    height: 0,
+    treeMap: {},
+    flatTreeMap: [],
+    flatTreeMapById: {},
+    list: {
+      expandedHeight: 0,
+      visibleRows: [],
+      rows: {},
+      width: 0
+    },
+    dimensions: {
+      width: 0,
+      height: 0
+    },
+    chart: {
+      dimensions: {
+        width: 0,
+        innerWidth: 0
+      },
+      visibleItems: [],
+      time: {
+        dates: {},
+        timePerPixel: 0,
+        firstTaskTime: 0,
+        lastTaskTime: 0,
+        totalViewDurationMs: 0,
+        totalViewDurationPx: 0,
+        leftGlobal: 0,
+        rightGlobal: 0,
+        leftPx: 0,
+        rightPx: 0,
+        leftInner: 0,
+        rightInner: 0,
+        maxWidth: {}
+      }
+    },
+    elements: {}
+  };
   if (typeof options.debug === 'boolean' && options.debug) {
     // @ts-ignore
     window.state = state;
