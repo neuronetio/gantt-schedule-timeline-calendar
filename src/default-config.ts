05de0b14595c16a6f5d4d5e0e8ae5a12cabe7657
neuronet.io <neuronet.io@gmail.com>
2019-10-22 14:42:42 +0200

    default config as function

--- src/default-config.ts
+++ src/default-config.ts
@@ -55,174 +55,177 @@ function generateEmptyActions() {
 }
 
 // default configuration
-const defaultConfig = {
-  height: 740,
-  headerHeight: 86,
-  components: {
-    Main,
-    List,
-    ListColumn,
-    ListColumnHeader,
-    ListColumnHeaderResizer,
-    ListColumnRow,
-    ListExpander,
-    ListToggle,
-    Chart,
-    ChartCalendar,
-    ChartCalendarDate,
-    ChartTimeline,
-    ChartTimelineGrid,
-    ChartTimelineGridBlock,
-    ChartTimelineGridRow,
-    ChartTimelineItems,
-    ChartTimelineItemsRow,
-    ChartTimelineItemsRowItem
-  },
-  wrappers: {
-    Main(input) {
-      return input;
+function defaultConfig() {
+  const actions = generateEmptyActions();
+  return {
+    height: 740,
+    headerHeight: 86,
+    components: {
+      Main,
+      List,
+      ListColumn,
+      ListColumnHeader,
+      ListColumnHeaderResizer,
+      ListColumnRow,
+      ListExpander,
+      ListToggle,
+      Chart,
+      ChartCalendar,
+      ChartCalendarDate,
+      ChartTimeline,
+      ChartTimelineGrid,
+      ChartTimelineGridBlock,
+      ChartTimelineGridRow,
+      ChartTimelineItems,
+      ChartTimelineItemsRow,
+      ChartTimelineItemsRowItem
     },
-    List(input) {
-      return input;
-    },
-    ListColumn(input) {
-      return input;
-    },
-    ListColumnHeader(input) {
-      return input;
-    },
-    ListColumnHeaderResizer(input) {
-      return input;
-    },
-    ListColumnRow(input) {
-      return input;
-    },
-    ListExpander(input) {
-      return input;
-    },
-    ListToggle(input) {
-      return input;
-    },
-    Chart(input) {
-      return input;
-    },
-    ChartCalendar(input) {
-      return input;
-    },
-    ChartCalendarDate(input) {
-      return input;
-    },
-    ChartTimeline(input) {
-      return input;
-    },
-    ChartTimelineGrid(input) {
-      return input;
-    },
-    ChartTimelineGridBlock(input) {
-      return input;
-    },
-    ChartTimelineGridRow(input) {
-      return input;
-    },
-    ChartTimelineItems(input) {
-      return input;
-    },
-    ChartTimelineItemsRow(input) {
-      return input;
-    },
-    ChartTimelineItemsRowItem(input) {
-      return input;
-    }
-  },
-  list: {
-    rows: {},
-    rowHeight: 40,
-    columns: {
-      percent: 100,
-      resizer: {
-        width: 10,
-        inRealTime: true,
-        dots: 6
+    wrappers: {
+      Main(input) {
+        return input;
       },
-      data: {}
-    },
-    expander: {
-      padding: 20,
-      size: 20,
-      icons: {
-        open:
-          '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>',
-        closed:
-          '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>'
+      List(input) {
+        return input;
+      },
+      ListColumn(input) {
+        return input;
+      },
+      ListColumnHeader(input) {
+        return input;
+      },
+      ListColumnHeaderResizer(input) {
+        return input;
+      },
+      ListColumnRow(input) {
+        return input;
+      },
+      ListExpander(input) {
+        return input;
+      },
+      ListToggle(input) {
+        return input;
+      },
+      Chart(input) {
+        return input;
+      },
+      ChartCalendar(input) {
+        return input;
+      },
+      ChartCalendarDate(input) {
+        return input;
+      },
+      ChartTimeline(input) {
+        return input;
+      },
+      ChartTimelineGrid(input) {
+        return input;
+      },
+      ChartTimelineGridBlock(input) {
+        return input;
+      },
+      ChartTimelineGridRow(input) {
+        return input;
+      },
+      ChartTimelineItems(input) {
+        return input;
+      },
+      ChartTimelineItemsRow(input) {
+        return input;
+      },
+      ChartTimelineItemsRowItem(input) {
+        return input;
       }
-    }
-  },
-  scroll: {
-    top: 0,
-    left: 0,
-    xMultiplier: 1.5,
-    yMultiplier: 1,
-    percent: {
+    },
+    list: {
+      rows: {},
+      rowHeight: 40,
+      columns: {
+        percent: 100,
+        resizer: {
+          width: 10,
+          inRealTime: true,
+          dots: 6
+        },
+        data: {}
+      },
+      expander: {
+        padding: 20,
+        size: 20,
+        icons: {
+          open:
+            '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>',
+          closed:
+            '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>'
+        }
+      }
+    },
+    scroll: {
       top: 0,
-      left: 0
-    }
-  },
-  chart: {
-    time: {
-      from: 0,
-      to: 0,
-      zoom: 21,
-      period: 'day',
-      dates: {},
-      maxWidth: {}
-    },
-    calendar: {
-      vertical: {
-        smallFormat: 'YYYY-MM-DD'
+      left: 0,
+      xMultiplier: 1.5,
+      yMultiplier: 1,
+      percent: {
+        top: 0,
+        left: 0
       }
     },
-    grid: {},
-    items: {}
-  },
-  classNames: {},
-  actions: generateEmptyActions(),
-  locale: {
-    name: 'en',
-    weekdays: 'Sunday_Monday_Tuesday_Wednesday_Thursday_Friday_Saturday'.split('_'),
-    weekdaysShort: 'Sun_Mon_Tue_Wed_Thu_Fri_Sat'.split('_'),
-    weekdaysMin: 'Su_Mo_Tu_We_Th_Fr_Sa'.split('_'),
-    months: 'January_February_March_April_May_June_July_August_September_October_November_December'.split('_'),
-    monthsShort: 'Jan_Feb_Mar_Apr_May_Jun_Jul_Aug_Sep_Oct_Nov_Dec'.split('_'),
-    weekStart: 1,
-    relativeTime: {
-      future: 'in %s',
-      past: '%s ago',
-      s: 'a few seconds',
-      m: 'a minute',
-      mm: '%d minutes',
-      h: 'an hour',
-      hh: '%d hours',
-      d: 'a day',
-      dd: '%d days',
-      M: 'a month',
-      MM: '%d months',
-      y: 'a year',
-      yy: '%d years'
+    chart: {
+      time: {
+        from: 0,
+        to: 0,
+        zoom: 21,
+        period: 'day',
+        dates: {},
+        maxWidth: {}
+      },
+      calendar: {
+        vertical: {
+          smallFormat: 'YYYY-MM-DD'
+        }
+      },
+      grid: {},
+      items: {}
     },
-    formats: {
-      LT: 'HH:mm',
-      LTS: 'HH:mm:ss',
-      L: 'DD/MM/YYYY',
-      LL: 'D MMMM YYYY',
-      LLL: 'D MMMM YYYY HH:mm',
-      LLLL: 'dddd, D MMMM YYYY HH:mm'
-    },
-    ordinal: n => {
-      const s = ['th', 'st', 'nd', 'rd'];
-      const v = n % 100;
-      return `[${n}${s[(v - 20) % 10] || s[v] || s[0]}]`;
+    classNames: {},
+    actions,
+    locale: {
+      name: 'en',
+      weekdays: 'Sunday_Monday_Tuesday_Wednesday_Thursday_Friday_Saturday'.split('_'),
+      weekdaysShort: 'Sun_Mon_Tue_Wed_Thu_Fri_Sat'.split('_'),
+      weekdaysMin: 'Su_Mo_Tu_We_Th_Fr_Sa'.split('_'),
+      months: 'January_February_March_April_May_June_July_August_September_October_November_December'.split('_'),
+      monthsShort: 'Jan_Feb_Mar_Apr_May_Jun_Jul_Aug_Sep_Oct_Nov_Dec'.split('_'),
+      weekStart: 1,
+      relativeTime: {
+        future: 'in %s',
+        past: '%s ago',
+        s: 'a few seconds',
+        m: 'a minute',
+        mm: '%d minutes',
+        h: 'an hour',
+        hh: '%d hours',
+        d: 'a day',
+        dd: '%d days',
+        M: 'a month',
+        MM: '%d months',
+        y: 'a year',
+        yy: '%d years'
+      },
+      formats: {
+        LT: 'HH:mm',
+        LTS: 'HH:mm:ss',
+        L: 'DD/MM/YYYY',
+        LL: 'D MMMM YYYY',
+        LLL: 'D MMMM YYYY HH:mm',
+        LLLL: 'dddd, D MMMM YYYY HH:mm'
+      },
+      ordinal: n => {
+        const s = ['th', 'st', 'nd', 'rd'];
+        const v = n % 100;
+        return `[${n}${s[(v - 20) % 10] || s[v] || s[0]}]`;
+      }
     }
-  }
-};
+  };
+}
 
-export default defaultConfig;
+export default defaultConfig();
