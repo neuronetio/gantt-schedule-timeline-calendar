18f318fba4cc676251ab4fa80e0652e500a499db
neuronet.io <neuronet.io@gmail.com>
2020-09-25 00:21:23 +0200

    selection plugin example updated

--- examples/selection-plugin/index.js
+++ examples/selection-plugin/index.js
@@ -43,7 +43,7 @@ const itemsFromDB = [
   },
   {
     id: '4',
-    label: 'Not selectable',
+    label: '🚫 Not selectable',
     rowId: '1',
     time: {
       start: GSTC.api.date('2020-01-05').startOf('day').valueOf(),
@@ -52,7 +52,7 @@ const itemsFromDB = [
   },
   {
     id: '5',
-    label: 'Not selectable',
+    label: '🚫 Not selectable',
     canSelect: false,
     rowId: '2',
     time: {
@@ -116,7 +116,7 @@ function addCellBackground({ time, row, vido }) {
   console.log('ceell', time.leftGlobalDate.format('YYYY-MM-DD'), isSelectable);
   return isSelectable
     ? vido.html`<div class="selectable-cell" style="width:100%;height:100%;"></div>`
-    : vido.html`<div class="not-selectable-cell" style="width:100%;height:100%;background: #ff000025;"></div>`;
+    : vido.html`<div class="not-selectable-cell" style="width:100%;height:100%;">🚫</div>`;
 }
 
 // Configuration object
@@ -126,7 +126,7 @@ const config = {
 
   licenseKey:
     '====BEGIN LICENSE KEY====\nXOfH/lnVASM6et4Co473t9jPIvhmQ/l0X3Ewog30VudX6GVkOB0n3oDx42NtADJ8HjYrhfXKSNu5EMRb5KzCLvMt/pu7xugjbvpyI1glE7Ha6E5VZwRpb4AC8T1KBF67FKAgaI7YFeOtPFROSCKrW5la38jbE5fo+q2N6wAfEti8la2ie6/7U2V+SdJPqkm/mLY/JBHdvDHoUduwe4zgqBUYLTNUgX6aKdlhpZPuHfj2SMeB/tcTJfH48rN1mgGkNkAT9ovROwI7ReLrdlHrHmJ1UwZZnAfxAC3ftIjgTEHsd/f+JrjW6t+kL6Ef1tT1eQ2DPFLJlhluTD91AsZMUg==||U2FsdGVkX1/SWWqU9YmxtM0T6Nm5mClKwqTaoF9wgZd9rNw2xs4hnY8Ilv8DZtFyNt92xym3eB6WA605N5llLm0D68EQtU9ci1rTEDopZ1ODzcqtTVSoFEloNPFSfW6LTIC9+2LSVBeeHXoLEQiLYHWihHu10Xll3KsH9iBObDACDm1PT7IV4uWvNpNeuKJc\npY3C5SG+3sHRX1aeMnHlKLhaIsOdw2IexjvMqocVpfRpX4wnsabNA0VJ3k95zUPS3vTtSegeDhwbl6j+/FZcGk9i+gAy6LuetlKuARjPYn2LH5Be3Ah+ggSBPlxf3JW9rtWNdUoFByHTcFlhzlU9HnpnBUrgcVMhCQ7SAjN9h2NMGmCr10Rn4OE0WtelNqYVig7KmENaPvFT+k2I0cYZ4KWwxxsQNKbjEAxJxrzK4HkaczCvyQbzj4Ppxx/0q+Cns44OeyWcwYD/vSaJm4Kptwpr+L4y5BoSO/WeqhSUQQ85nvOhtE0pSH/ZXYo3pqjPdQRfNm6NFeBl2lwTmZUEuw==\n====END LICENSE KEY====',
-
+  innerHeight: 100,
   plugins: [
     TimelinePointer({}),
     Selection({
@@ -196,6 +196,33 @@ window.state = state;
 const app = GSTC({
   element: document.getElementById('gstc'),
   state,
+});
+
+document
+  .getElementById('select-items')
+  .addEventListener('click', function (el) {
+    app.api.plugins.selection.selectItems([GSTC.api.GSTCID('3')]);
+  });
+document
+  .getElementById('select-cells')
+  .addEventListener('click', function (el) {
+    const allCells = app.api.getGridCells();
+    const cellsForSelect = allCells
+      .filter(
+        (cell) => cell.time.leftGlobalDate.format('YYYY-MM-DD') === '2020-01-09'
+      )
+      .map((cell) => cell.id);
+    app.api.plugins.selection.selectCells(cellsForSelect);
+  });
+document
+  .getElementById('get-selected')
+  .addEventListener('click', function (el) {
+    const selected = app.api.plugins.selection.getSelected();
+    console.log('selected', selected);
+  });
+document.getElementById('clear').addEventListener('click', function (el) {
+  app.api.plugins.selection.selectCells([]);
+  app.api.plugins.selection.selectItems([]);
 });
 
 //for testing
