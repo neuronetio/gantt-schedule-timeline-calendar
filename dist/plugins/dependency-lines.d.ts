b470000df38d1f3a735fa54a4c61d520a4c96534
neuronet.io <neuronet.io@gmail.com>
2020-09-27 00:34:56 +0200

    v3.0.66

--- dist/plugins/dependency-lines.d.ts
+++
@@ -1,6 +0,0 @@
-import { Vido } from '../gstc';
-export interface Options {
-    type?: 'straight';
-    propertyName?: string;
-}
-export declare function Plugin(options?: Options): (vidoInstance: Vido) => () => void;
--- dist/plugins/progress-bar.d.ts
+++
@@ -1,7 +0,0 @@
-import { Vido } from '../gstc';
-export declare const pluginPath = "config.plugin.ProgressBar";
-export interface Options {
-    enabled?: boolean;
-    className?: string;
-}
-export declare function Plugin(options?: Options): (vidoInstance: Vido) => () => void;
