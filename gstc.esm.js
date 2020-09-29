62d60bd7c26f2d8c3ada955e7f277c3aeeb9db1b
neuronet.io <neuronet.io@gmail.com>
2020-09-29 23:11:16 +0200

    input gstc.esm.js experiment

---
+++ gstc.esm.js
@@ -0,0 +1,11 @@
+/**
+ * Gantt-Schedule-Timeline-Calendar
+ *
+ * @copyright NEURONET - Rafal Pospiech <https://neuronet.io>
+ * @author    Rafal Pospiech <neuronet.io@gmail.com>
+ * @package   gantt-schedule-timeline-calendar
+ * @version   3.1.1
+ * @license   NEURONET Free License (see https://github.com/neuronetio/gantt-schedule-timeline-calendar/blob/master/LICENSE)
+ */
+import GSTC from './dist/gstc.esm.min.js';
+export default GSTC;
