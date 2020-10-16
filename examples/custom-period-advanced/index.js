84399fb3206f71c97e3f8c86d2a61ed4dad14b33
neuronet.io <neuronet.io@gmail.com>
2020-10-16 05:26:20 +0200

    custom period advanced example

---
+++ examples/custom-period-advanced/index.js
@@ -0,0 +1,150 @@
+import GSTC from '../../dist/gstc.esm.min.js';
+// @ts-ignore
+GSTC.api.dayjs.extend(window.dayjs_plugin_weekOfYear);
+//@ts-ignore
+GSTC.api.dayjs.extend(window.dayjs_plugin_advancedFormat);
+
+const rowsFromDB = [
+  {
+    id: '1',
+    label: 'Row 1',
+  },
+  {
+    id: '2',
+    label: 'Row 2',
+  },
+];
+
+const itemsFromDB = [
+  {
+    id: '1',
+    label: 'Item 1',
+    rowId: '1',
+    time: {
+      start: GSTC.api.date('2020-02-01').startOf('day').valueOf(),
+      end: GSTC.api.date('2020-02-06').endOf('day').valueOf(),
+    },
+  },
+  {
+    id: '2',
+    label: 'Item 2',
+    rowId: '1',
+    time: {
+      start: GSTC.api.date('2020-03-01').startOf('day').valueOf(),
+      end: GSTC.api.date('2020-03-15').endOf('day').valueOf(),
+    },
+  },
+  {
+    id: '3',
+    label: 'Item 3',
+    rowId: '2',
+    time: {
+      start: GSTC.api.date('2020-02-15').startOf('day').valueOf(),
+      end: GSTC.api.date('2020-02-20').endOf('day').valueOf(),
+    },
+  },
+  {
+    id: '4',
+    label: 'Item 4',
+    rowId: '2',
+    time: {
+      start: GSTC.api.date('2020-04-15').startOf('day').valueOf(),
+      end: GSTC.api.date('2020-04-20').endOf('day').valueOf(),
+    },
+  },
+];
+
+const columnsFromDB = [
+  {
+    id: 'id',
+    label: 'ID',
+    data: ({ row }) => Number(GSTC.api.sourceID(row.id)), // show original id
+    sortable: ({ row }) => Number(GSTC.api.sourceID(row.id)), // sort by id converted to number
+    width: 80,
+    header: {
+      content: 'ID',
+    },
+  },
+  {
+    id: 'label',
+    data: 'label',
+    sortable: 'label',
+    isHTML: false,
+    width: 300,
+    header: {
+      content: 'Label',
+    },
+  },
+];
+
+const months = [
+  {
+    zoomTo: 100, // we want to display this format for all zoom levels until 100
+    period: 'month',
+    format({ timeStart }) {
+      return timeStart.format('MMMM YYYY'); // full list of formats: https://day.js.org/docs/en/display/format
+    },
+  },
+];
+
+const days = [
+  {
+    zoomTo: 100, // we want to display this format for all zoom levels until 100
+    period: 'day',
+    periodIncrement({ currentDates, leftDate }) {
+      if (!currentDates.length) return Math.ceil(leftDate.endOf('month').date() / 2);
+      const lastDate = currentDates[currentDates.length - 1];
+      if (lastDate.leftGlobalDate.date() === 1) {
+        // we are in the middle of the month
+        return Math.floor(lastDate.leftGlobalDate.endOf('month').date() / 2);
+      }
+      // we are at the beginning - so we can take previous date and add month to get current month
+      return Math.ceil(lastDate.leftGlobalDate.add(1, 'month').endOf('month').date() / 2);
+    },
+    main: true,
+    format({ timeStart, timeEnd }) {
+      return timeStart.format('DD') + ' - ' + timeEnd.format('DD');
+    },
+  },
+];
+
+// Configuration object
+const config = {
+  // for free key for your domain please visit https://gstc.neuronet.io/free-key
+  // if you need commercial license please visit https://gantt-schedule-timeline-calendar.neuronet.io/pricing
+
+  licenseKey:
+    '====BEGIN LICENSE KEY====\nXOfH/lnVASM6et4Co473t9jPIvhmQ/l0X3Ewog30VudX6GVkOB0n3oDx42NtADJ8HjYrhfXKSNu5EMRb5KzCLvMt/pu7xugjbvpyI1glE7Ha6E5VZwRpb4AC8T1KBF67FKAgaI7YFeOtPFROSCKrW5la38jbE5fo+q2N6wAfEti8la2ie6/7U2V+SdJPqkm/mLY/JBHdvDHoUduwe4zgqBUYLTNUgX6aKdlhpZPuHfj2SMeB/tcTJfH48rN1mgGkNkAT9ovROwI7ReLrdlHrHmJ1UwZZnAfxAC3ftIjgTEHsd/f+JrjW6t+kL6Ef1tT1eQ2DPFLJlhluTD91AsZMUg==||U2FsdGVkX1/SWWqU9YmxtM0T6Nm5mClKwqTaoF9wgZd9rNw2xs4hnY8Ilv8DZtFyNt92xym3eB6WA605N5llLm0D68EQtU9ci1rTEDopZ1ODzcqtTVSoFEloNPFSfW6LTIC9+2LSVBeeHXoLEQiLYHWihHu10Xll3KsH9iBObDACDm1PT7IV4uWvNpNeuKJc\npY3C5SG+3sHRX1aeMnHlKLhaIsOdw2IexjvMqocVpfRpX4wnsabNA0VJ3k95zUPS3vTtSegeDhwbl6j+/FZcGk9i+gAy6LuetlKuARjPYn2LH5Be3Ah+ggSBPlxf3JW9rtWNdUoFByHTcFlhzlU9HnpnBUrgcVMhCQ7SAjN9h2NMGmCr10Rn4OE0WtelNqYVig7KmENaPvFT+k2I0cYZ4KWwxxsQNKbjEAxJxrzK4HkaczCvyQbzj4Ppxx/0q+Cns44OeyWcwYD/vSaJm4Kptwpr+L4y5BoSO/WeqhSUQQ85nvOhtE0pSH/ZXYo3pqjPdQRfNm6NFeBl2lwTmZUEuw==\n====END LICENSE KEY====',
+
+  list: {
+    columns: {
+      data: GSTC.api.fromArray(columnsFromDB),
+    },
+    rows: GSTC.api.fromArray(rowsFromDB),
+  },
+  chart: {
+    items: GSTC.api.fromArray(itemsFromDB),
+    calendarLevels: [months, days],
+    time: {
+      zoom: 24,
+      from: GSTC.api.date('2020-01-01'),
+    },
+  },
+};
+
+// Generate GSTC state from configuration object
+const state = GSTC.api.stateFromConfig(config);
+
+// for testing
+// @ts-ignore
+window.state = state;
+
+// Mount the component
+const app = GSTC({
+  element: document.getElementById('gstc'),
+  state,
+});
+
+//for testing
+// @ts-ignore
+window.gstc = app;
