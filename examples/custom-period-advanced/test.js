3984cd2744d6686b262dcd075183a5675c3b9502
neuronet.io <neuronet.io@gmail.com>
2020-10-16 05:39:03 +0200

    custom-period-advanced test updated

--- examples/custom-period-advanced/test.js
+++ examples/custom-period-advanced/test.js
@@ -8,8 +8,8 @@ describe('Basic', () => {
   });
 
   it('should display weeks', async () => {
-    // await expect(page).toMatchElement('.gstc__chart-calendar-date-content--week', {
-    //   text: '27th to 9th, week 05 to 06',
-    // });
+    await expect(page).toMatchElement('.gstc__chart-calendar-date-content', {
+      text: '01 - 16',
+    });
   });
 });
