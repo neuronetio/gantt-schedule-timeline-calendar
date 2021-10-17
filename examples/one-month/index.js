1e420c17cec4e9c8a48eff10fdb35182d54dd740
neuronetio <neuronet.io@gmail.com>
2021-10-17 19:58:06 +0200

    one-month example added

---
+++ examples/one-month/index.js
@@ -0,0 +1,461 @@
+import GSTC from '../../dist/gstc.esm.min.js';
+import { Plugin as TimelinePointer } from '../../dist/plugins/timeline-pointer.esm.min.js';
+import { Plugin as Selection } from '../../dist/plugins/selection.esm.min.js';
+import { Plugin as ItemMovement } from '../../dist/plugins/item-movement.esm.min.js';
+import { Plugin as ItemResizing } from '../../dist/plugins/item-resizing.esm.min.js';
+import { Plugin as CalendarScroll } from '../../dist/plugins/calendar-scroll.esm.min.js';
+import { Plugin as HighlightWeekends } from '../../dist/plugins/highlight-weekends.esm.min.js';
+import { Plugin as ProgressBar } from '../../dist/plugins/progress-bar.esm.min.js';
+import { Plugin as TimeBookmarks } from '../../dist/plugins/time-bookmarks.esm.min.js';
+import { Plugin as DependencyLines } from '../../dist/plugins/dependency-lines.esm.min.js';
+
+const iterations = 100;
+const GSTCID = GSTC.api.GSTCID;
+
+function getRandomFaceImage() {
+  return `./faces/face-${Math.ceil(Math.random() * 50)}.jpg`;
+}
+
+const colors = ['#E74C3C', '#DA3C78', '#7E349D', '#0077C0', '#07ABA0', '#0EAC51', '#F1892D'];
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+const startDate = GSTC.api.date().startOf('month');
+const endDate = startDate.clone().endOf('month');
+const startTime = startDate.valueOf();
+
+const rows = {};
+for (let i = 0; i < iterations; i++) {
+  const withParent = i > 0 && i % 2 === 0;
+  const id = GSTCID(String(i));
+  rows[id] = {
+    id,
+    label: `John Doe ${i}`,
+    parentId: withParent ? GSTCID(String(i - 1)) : undefined,
+    expanded: false,
+    vacations: [],
+    img: getRandomFaceImage(),
+    progress: Math.floor(Math.random() * 100),
+  };
+}
+
+rows[GSTCID('3')].vacations = [
+  startDate.add(5, 'days').startOf('day').format('YYYY-MM-DD'),
+  startDate.add(6, 'days').startOf('day').format('YYYY-MM-DD'),
+];
+
+rows[GSTCID('11')].label = 'NESTED TREE HERE';
+rows[GSTCID('12')].parentId = GSTCID('11');
+rows[GSTCID('13')].parentId = GSTCID('12');
+rows[GSTCID('14')].parentId = GSTCID('13');
+
+rows[GSTCID('7')].birthday = startDate.add(3, 'day').startOf('day').format('YYYY-MM-DD');
+
+const items = {};
+for (let i = 0; i < iterations; i++) {
+  let rowId = GSTCID(i);
+  let id = GSTCID(i);
+  let startDayjs = GSTC.api
+    .date(startTime)
+    .startOf('month')
+    .add(Math.floor(Math.random() * 30), 'day');
+  items[id] = {
+    id,
+    label: `John Doe ${i}`,
+    progress: Math.round(Math.random() * 100),
+    style: { background: getRandomColor() },
+    time: {
+      start: startDayjs.startOf('day').valueOf(),
+      end: startDayjs
+        .clone()
+        .add(Math.floor(Math.random() * 20) + 4, 'day')
+        .endOf('day')
+        .valueOf(),
+    },
+    rowId,
+    img: getRandomFaceImage(),
+    description: 'Lorem ipsum dolor sit amet',
+  };
+}
+
+items[GSTCID('0')].linkedWith = [GSTCID('1')];
+items[GSTCID('0')].label = 'Task 0 linked with 1';
+items[GSTCID('0')].type = 'task';
+items[GSTCID('1')].label = 'Task 1 linked with 0';
+items[GSTCID('1')].type = 'task';
+items[GSTCID('1')].time = { ...items[GSTCID('0')].time };
+
+items[GSTCID('0')].style = { background: colors[3] };
+items[GSTCID('1')].style = { background: colors[3] };
+
+items[GSTCID('3')].dependant = [GSTCID('5')];
+items[GSTCID('3')].label = 'Grab and move me into vacation area';
+items[GSTCID('3')].time.start = GSTC.api.date(startTime).add(4, 'day').startOf('day').add(5, 'day').valueOf();
+items[GSTCID('3')].time.end = GSTC.api.date(items[GSTCID('3')].time.start).endOf('day').add(5, 'day').valueOf();
+
+items[GSTCID('5')].time.start = GSTC.api.date(items[GSTCID('3')].time.end).startOf('day').add(5, 'day').valueOf();
+items[GSTCID('5')].time.end = GSTC.api.date(items[GSTCID('5')].time.start).endOf('day').add(2, 'day').valueOf();
+items[GSTCID('5')].dependant = [GSTCID('7'), GSTCID('9')];
+
+items[GSTCID('7')].time.start = GSTC.api.date(items[GSTCID('5')].time.end).startOf('day').add(3, 'day').valueOf();
+items[GSTCID('7')].time.end = GSTC.api.date(items[GSTCID('7')].time.start).endOf('day').add(2, 'day').valueOf();
+items[GSTCID('9')].time.start = GSTC.api.date(items[GSTCID('5')].time.end).startOf('day').add(2, 'day').valueOf();
+items[GSTCID('9')].time.end = GSTC.api.date(items[GSTCID('9')].time.start).endOf('day').add(3, 'day').valueOf();
+
+const columns = {
+  data: {
+    [GSTCID('id')]: {
+      id: GSTCID('id'),
+      data: ({ row }) => GSTC.api.sourceID(row.id),
+      width: 80,
+      sortable: ({ row }) => Number(GSTC.api.sourceID(row.id)),
+      header: {
+        content: 'ID',
+      },
+    },
+    [GSTCID('label')]: {
+      id: GSTCID('label'),
+      data: 'label',
+      sortable: 'label',
+      expander: true,
+      isHTML: false,
+      width: 335,
+      header: {
+        content: 'Label',
+      },
+    },
+    [GSTCID('progress')]: {
+      id: GSTCID('progress'),
+      data({ row, vido }) {
+        return vido.html`<div style="text-align:center">${row.progress}</div>`;
+      },
+      width: 80,
+      sortable: 'progress',
+      header: {
+        content: 'Progress',
+      },
+    },
+  },
+};
+
+const bookmarks = {
+  now: {
+    time: GSTC.api.date().valueOf(),
+    color: '#3498DB',
+    label: 'Now',
+  },
+};
+
+for (let i = 0; i < 50; i++) {
+  const id = `Bookmark ${i}`;
+  bookmarks[id] = {
+    time: GSTC.api
+      .date()
+      .add(Math.round(Math.random() * 600) - 300, 'day')
+      .startOf('day')
+      .valueOf(),
+    label: id,
+  };
+}
+
+function itemSlot(vido, props) {
+  const { html, onChange, update } = vido;
+
+  let imageSrc = '';
+  let description = '';
+  onChange((newProps) => {
+    props = newProps;
+    if (!props) return;
+    imageSrc = props.item.img;
+    description = props.item.description;
+    update();
+  });
+
+  return (content) =>
+    html`<div
+        class="item-image"
+        style="background:url(${imageSrc}),transparent;flex-shrink:0;border-radius:100%;width:34px;height:34px;vertical-align: middle;background-size: 100%;margin: 4px 11px 0px 0px;"
+      ></div>
+      <div class="item-text">
+        <div class="item-label">${content}</div>
+        <div class="item-description" style="font-size:11px;margin-top:2px;color:#fffffff0;line-height:1em;">
+          ${description}
+        </div>
+      </div>`;
+}
+
+function rowSlot(vido, props) {
+  const { html, onChange, update, api } = vido;
+
+  let img = '';
+  onChange((newProps) => {
+    props = newProps;
+    if (!props) return;
+    img = props.row.img;
+    update();
+  });
+
+  return (content) =>
+    api.sourceID(props.column.id) === 'label'
+      ? html`<div class="row-content-wrapper" style="display:flex">
+          <div class="row-content" style="flex-grow:1;">${content}</div>
+          <div
+            class="row-image"
+            style="background:url(${img}),transparent;border-radius:100%;width:34px;height:34px;vertical-align: middle;background-size: 100%;margin: 11px 11px 0px 0px;"
+          ></div>
+        </div>`
+      : content;
+}
+
+function onCellCreateVacation({ time, row, vido, content }) {
+  if (row.vacations.includes(time.leftGlobalDate.format('YYYY-MM-DD'))) {
+    return vido.html`<div title="🏖️ VACATION" style="height:100%"><div style="font-size:11px;background:#A0A0A0;color:white;">Vacation</div></div>${content}`;
+  }
+  return content;
+}
+
+function onCellCreateBirthday({ time, row, vido, content }) {
+  if (row.birthday === time.leftGlobalDate.format('YYYY-MM-DD')) {
+    return vido.html`${content}<div title="🎁 BIRTHDAY" style="height:100%;font-size:18px;"><div style="font-size:11px;background:#F9B32F;color:white;margin-bottom:10px;">🎁 Birthday</div></div>`;
+  }
+  return content;
+}
+
+function mainOuterSlot(vido, props) {
+  const { onChange, api, update, html, state, getElement } = vido;
+
+  onChange((changedProps) => {
+    // if current element is reused to display other item data just update your data so when you click you will display right alert
+    props = changedProps;
+  });
+
+  let year = api.time.date().year();
+  let month = api.time.date().month();
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  let loading = '';
+  let overlay = '';
+
+  function updateTime() {
+    if (loading) return;
+    const startTime = api.time
+      .date(`${year}-${month + 1}-01`)
+      .startOf('month')
+      .valueOf();
+    const endTime = api.time
+      .date(`${year}-${month + 1}-01`)
+      .endOf('month')
+      .valueOf();
+    loading = 'You can load items from backend now.';
+    overlay = 'overlay';
+    setTimeout(() => {
+      // if you have items you can change view
+      state.update('config.chart.time', (time) => {
+        time.from = startTime;
+        time.to = endTime;
+        console.log(`${year}-${month + 1}-01`, `${year}-${month + 1}-01`);
+        return time;
+      });
+      loading = '';
+      overlay = '';
+    }, 1000);
+  }
+
+  let listenerAdded = false;
+  function getEl(element) {
+    if (listenerAdded) return;
+    element.addEventListener('change', (ev) => {
+      if (month !== ev.target.value) {
+        month = Number(ev.target.value);
+        updateTime();
+        update();
+      }
+    });
+    listenerAdded = true;
+  }
+
+  function setPrevYear() {
+    if (loading) return;
+    year -= 1;
+    updateTime();
+    update();
+  }
+
+  function setNextYear() {
+    if (loading) return;
+    year += 1;
+    updateTime();
+    update();
+  }
+
+  function setPrevMonth() {
+    if (loading) return;
+    month -= 1;
+    if (month < 0) {
+      month = 11;
+      year--;
+    }
+    updateTime();
+    update();
+  }
+
+  function setNextMonth() {
+    if (loading) return;
+    month += 1;
+    if (month > 11) {
+      month = 0;
+      year++;
+    }
+    updateTime();
+    update();
+  }
+
+  // return render function
+  return (content) =>
+    html`<div class="tool-shelf" style="margin:20px;">
+      Year: <button style="margin-left:20px;" @click=${setPrevYear}><</button><input type="number" .value=${year}><button style="margin-right:20px;" @click=${setNextYear}>></button>
+      Month: <button style="margin-left:20px;" @click=${setPrevMonth}><</button><select get-element=${getElement(
+      getEl
+    )}>${months.map(
+      (monthText, index) => html`<option value=${index} ?selected=${index === month}>${monthText}</option>`
+    )}</option></select><button style="margin-right:20px;" @click=${setNextMonth}>></button>
+    </div>${content}<div class=${overlay}>LOADING... ${loading}</div>`;
+}
+
+const itemMovementOptions = {
+  events: {
+    onMove({ items }) {
+      for (const item of items.after) {
+        const row = gstc.api.getRow(item.rowId);
+        for (const vacation of row.vacations) {
+          const vacationStart = gstc.api.time.date(vacation).startOf('day').valueOf();
+          const vacationEnd = gstc.api.time.date(vacation).endOf('day').valueOf();
+          // item start time inside vacation
+          if (item.time.start >= vacationStart && item.time.start <= vacationEnd) return items.before;
+          // item end time inside vacation
+          if (item.time.end >= vacationStart && item.time.end <= vacationEnd) return items.before;
+          // vacation is between item start and end
+          if (item.time.start <= vacationStart && item.time.end >= vacationEnd) return items.before;
+          // item start and end time is inside vacation
+          if (item.time.start >= vacationStart && item.time.end <= vacationEnd) return items.before;
+        }
+      }
+      return items.after;
+    },
+  },
+};
+
+const itemResizeOptions = {
+  events: {
+    onResize({ items }) {
+      for (const item of items.after) {
+        const row = gstc.api.getRow(item.rowId);
+        for (const vacation of row.vacations) {
+          const vacationStart = gstc.api.time.date(vacation).startOf('day').valueOf();
+          const vacationEnd = gstc.api.time.date(vacation).endOf('day').valueOf();
+          // item start time inside vacation
+          if (item.time.start >= vacationStart && item.time.start <= vacationEnd) return items.before;
+          // item end time inside vacation
+          if (item.time.end >= vacationStart && item.time.end <= vacationEnd) return items.before;
+          // vacation is between item start and end
+          if (item.time.start <= vacationStart && item.time.end >= vacationEnd) return items.before;
+          // item start and end time is inside vacation
+          if (item.time.start >= vacationStart && item.time.end <= vacationEnd) return items.before;
+        }
+      }
+      return items.after;
+    },
+  },
+};
+
+const config = {
+  //debug: true,
+  licenseKey:
+    '====BEGIN LICENSE KEY====\nXOfH/lnVASM6et4Co473t9jPIvhmQ/l0X3Ewog30VudX6GVkOB0n3oDx42NtADJ8HjYrhfXKSNu5EMRb5KzCLvMt/pu7xugjbvpyI1glE7Ha6E5VZwRpb4AC8T1KBF67FKAgaI7YFeOtPFROSCKrW5la38jbE5fo+q2N6wAfEti8la2ie6/7U2V+SdJPqkm/mLY/JBHdvDHoUduwe4zgqBUYLTNUgX6aKdlhpZPuHfj2SMeB/tcTJfH48rN1mgGkNkAT9ovROwI7ReLrdlHrHmJ1UwZZnAfxAC3ftIjgTEHsd/f+JrjW6t+kL6Ef1tT1eQ2DPFLJlhluTD91AsZMUg==||U2FsdGVkX1/SWWqU9YmxtM0T6Nm5mClKwqTaoF9wgZd9rNw2xs4hnY8Ilv8DZtFyNt92xym3eB6WA605N5llLm0D68EQtU9ci1rTEDopZ1ODzcqtTVSoFEloNPFSfW6LTIC9+2LSVBeeHXoLEQiLYHWihHu10Xll3KsH9iBObDACDm1PT7IV4uWvNpNeuKJc\npY3C5SG+3sHRX1aeMnHlKLhaIsOdw2IexjvMqocVpfRpX4wnsabNA0VJ3k95zUPS3vTtSegeDhwbl6j+/FZcGk9i+gAy6LuetlKuARjPYn2LH5Be3Ah+ggSBPlxf3JW9rtWNdUoFByHTcFlhzlU9HnpnBUrgcVMhCQ7SAjN9h2NMGmCr10Rn4OE0WtelNqYVig7KmENaPvFT+k2I0cYZ4KWwxxsQNKbjEAxJxrzK4HkaczCvyQbzj4Ppxx/0q+Cns44OeyWcwYD/vSaJm4Kptwpr+L4y5BoSO/WeqhSUQQ85nvOhtE0pSH/ZXYo3pqjPdQRfNm6NFeBl2lwTmZUEuw==\n====END LICENSE KEY====',
+  innerHeight: 500,
+  plugins: [
+    HighlightWeekends(),
+    TimelinePointer(), // timeline pointer must go first before selection, resizing and movement
+    Selection(),
+    ItemResizing(itemResizeOptions), // resizing must fo before movement
+    ItemMovement(itemMovementOptions),
+    CalendarScroll(),
+    ProgressBar(),
+    TimeBookmarks({
+      bookmarks,
+    }),
+    DependencyLines({
+      onLine: [
+        (line) => {
+          line.type = GSTC.api.sourceID(line.fromItem.id) === '3' ? 'smooth' : 'square';
+          return line;
+        },
+      ],
+    }),
+  ],
+  list: {
+    row: {
+      height: 68,
+    },
+    rows,
+    columns,
+  },
+  chart: {
+    time: {
+      calculatedZoomMode: true,
+      from: startDate.valueOf(),
+      to: endDate.valueOf(),
+    },
+    item: {
+      height: 50,
+      gap: {
+        top: 14,
+        bottom: 0,
+      },
+    },
+    items,
+    grid: {
+      cell: {
+        onCreate: [onCellCreateVacation, onCellCreateBirthday],
+      },
+    },
+  },
+  scroll: {
+    vertical: { precise: true },
+  },
+  slots: {
+    'chart-timeline-items-row-item': { content: [itemSlot] },
+    'list-column-row': { content: [rowSlot] },
+    main: { outer: [mainOuterSlot] },
+  },
+};
+
+let gstc;
+let state = GSTC.api.stateFromConfig(config);
+(async function mountGSTC() {
+  const element = document.getElementById('gstc');
+
+  gstc = GSTC({
+    element,
+    state,
+  });
+
+  //@ts-ignore
+  window.state = state;
+  //@ts-ignore
+  window.gstc = gstc;
+})();
