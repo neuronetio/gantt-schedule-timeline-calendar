6dc1ec84a236b935010ad6698c523b2242a8b0a1
neuronetio <neuronet.io@gmail.com>
2024-04-26 09:57:33 +0200

    wasm as default file (examples)

--- examples/plugin-item-image/index.js
+++ examples/plugin-item-image/index.js
@@ -1,4 +1,4 @@
-import GSTC from '../../dist/gstc.esm.min.js';
+import GSTC from '../../dist/gstc.wasm.esm.min.js';
 // or when you encounter problems with wasm loader
 // import GSTC from '../../dist/gstc.wasm.esm.min.js';
 import { Plugin as ItemImage } from './item-image.js';
